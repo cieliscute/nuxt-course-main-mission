f9ee60a1ef22648199333a59fe3320f8f5fd45cb
kaiwei <kaiwei.depo@gmail.com>
2024-11-12 15:56:22 +0800

    test: 💍 tmpsave

--- pages/account/Login.vue
+++ pages/account/Login.vue
@@ -1,8 +1,11 @@
 <script setup>
 </script>
 <template>
-  <div>
-    AccountLogin
-  </div>
+  <p class="mb-2 text-primary-100 fs-8 fs-md-7 fw-bold">
+    享樂酒店，誠摯歡迎
+  </p>
+  <h1 class="text-neutral-0 fw-bold">
+    立即開始旅程
+  </h1>
 </template>
 <style scoped></style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout:'another'
+  layout:'dashboard'
 })
 </script>
 
